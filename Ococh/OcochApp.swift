541bb114b6e37037668782e2fe7fcd06244eeb7a
Kristofer <kris@youngers.org>
2022-08-29 12:59:22 -0400

    Initial Commit

---
+++ Ococh/OcochApp.swift
@@ -0,0 +1,20 @@
+//
+//  OcochApp.swift
+//  Ococh
+//
+//  Created by Kristofer Younger on 8/29/22.
+//
+
+import SwiftUI
+
+@main
+struct OcochApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
